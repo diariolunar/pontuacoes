1a7ce8f6f9aaad56c787334f84839cabf80e0a85
diariolunar <diariolunar47@gmail.com>
2026-05-13 16:26:59 -0300

    Create membros.service.js

---
+++ js/services/membros.service.js
@@ -0,0 +1,44 @@
+import { db } from "../config/firebase.js";
+
+import {
+  doc,
+  getDoc,
+  setDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+import {
+  criarIdSeguro,
+  normalizarUser
+} from "../core/utils.js";
+
+export async function buscarOuCriarMembro({ nome, user, sub = "" }) {
+  const userNormalizado = normalizarUser(user);
+  const membroId = criarIdSeguro(userNormalizado);
+
+  const membroRef = doc(db, "membros", membroId);
+  const membroSnap = await getDoc(membroRef);
+
+  if (membroSnap.exists()) {
+    return {
+      id: membroId,
+      ...membroSnap.data()
+    };
+  }
+
+  const novoMembro = {
+    nome,
+    user: userNormalizado,
+    sub,
+    status: "ativo",
+    criadoEm: serverTimestamp(),
+    atualizadoEm: serverTimestamp()
+  };
+
+  await setDoc(membroRef, novoMembro);
+
+  return {
+    id: membroId,
+    ...novoMembro
+  };
+}
