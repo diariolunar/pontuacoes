6a9618a63f8a8c65a7e98b0d5bcad448f242a77c
diariolunar <diariolunar47@gmail.com>
2026-05-13 16:27:17 -0300

    Create pontuacoes.service.js

---
+++ js/services/pontuacoes.service.js
@@ -0,0 +1,142 @@
+import { db } from "../config/firebase.js";
+
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  increment,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+import {
+  criarIdSeguro,
+  normalizarUser
+} from "../core/utils.js";
+
+import {
+  buscarOuCriarMembro
+} from "./membros.service.js";
+
+export async function registrarPontuacaoSub({
+  sub,
+  codigoAdm,
+  semana,
+  membros
+}) {
+  const envioRef = await addDoc(collection(db, "enviosSubs"), {
+    sub,
+    codigoAdm,
+    semana,
+    totalMembros: membros.length,
+    criadoEm: serverTimestamp()
+  });
+
+  for (const membro of membros) {
+    const userNormalizado = normalizarUser(membro.user);
+
+    await buscarOuCriarMembro({
+      nome: membro.nome,
+      user: userNormalizado,
+      sub
+    });
+
+    await addDoc(collection(db, "pontuacoesSubs"), {
+      envioId: envioRef.id,
+      semana,
+      sub,
+      nome: membro.nome,
+      user: userNormalizado,
+      pontos: Number(membro.pontos),
+      criadoEm: serverTimestamp()
+    });
+
+    await somarPontuacaoGeral({
+      semana,
+      nome: membro.nome,
+      user: userNormalizado,
+      categoria: "subs",
+      pontos: Number(membro.pontos),
+      origem: sub
+    });
+  }
+
+  return envioRef.id;
+}
+
+export async function somarPontuacaoGeral({
+  semana,
+  nome,
+  user,
+  categoria,
+  pontos,
+  origem = ""
+}) {
+  const userNormalizado = normalizarUser(user);
+  const rankingId = `${criarIdSeguro(semana)}_${criarIdSeguro(userNormalizado)}`;
+
+  const rankingRef = doc(db, "pontuacaoGeral", rankingId);
+  const rankingSnap = await getDoc(rankingRef);
+
+  const campoCategoria = `total_${categoria}`;
+
+  if (!rankingSnap.exists()) {
+    await setDoc(rankingRef, {
+      semana,
+      nome,
+      user: userNormalizado,
+      [campoCategoria]: pontos,
+      totalGeral: pontos,
+      atualizadoEm: serverTimestamp()
+    });
+  } else {
+    await updateDoc(rankingRef, {
+      [campoCategoria]: increment(pontos),
+      totalGeral: increment(pontos),
+      atualizadoEm: serverTimestamp()
+    });
+  }
+
+  await addDoc(collection(db, "historicoPontuacoes"), {
+    semana,
+    nome,
+    user: userNormalizado,
+    categoria,
+    pontos,
+    origem,
+    criadoEm: serverTimestamp()
+  });
+}
+
+export async function listarUltimosEnviosSubs() {
+  const consulta = query(
+    collection(db, "enviosSubs"),
+    orderBy("criadoEm", "desc")
+  );
+
+  const snapshot = await getDocs(consulta);
+
+  return snapshot.docs.map((documento) => ({
+    id: documento.id,
+    ...documento.data()
+  }));
+}
+
+export async function listarPontuacaoGeral() {
+  const consulta = query(
+    collection(db, "pontuacaoGeral"),
+    orderBy("totalGeral", "desc")
+  );
+
+  const snapshot = await getDocs(consulta);
+
+  return snapshot.docs.map((documento) => ({
+    id: documento.id,
+    ...documento.data()
+  }));
+}
