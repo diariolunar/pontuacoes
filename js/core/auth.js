4152a9257483c0c7b3d5dfdbbdbcbaeddc50751f
diariolunar <diariolunar47@gmail.com>
2026-05-13 16:26:21 -0300

    Create auth.js

---
+++ js/core/auth.js
@@ -0,0 +1,44 @@
+import { auth } from "../config/firebase.js";
+
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+
+export async function fazerLogin(email, senha) {
+  return await signInWithEmailAndPassword(auth, email, senha);
+}
+
+export async function fazerLogout() {
+  await signOut(auth);
+  window.location.href = "./login.html";
+}
+
+export function protegerPagina() {
+  onAuthStateChanged(auth, (usuario) => {
+    if (!usuario) {
+      window.location.href = "./login.html";
+    }
+  });
+}
+
+export function redirecionarSeLogado() {
+  onAuthStateChanged(auth, (usuario) => {
+    if (usuario) {
+      window.location.href = "./admin.html";
+    }
+  });
+}
+
+export function configurarBotaoLogout() {
+  setTimeout(() => {
+    const logoutBtn = document.getElementById("logoutBtn");
+
+    if (!logoutBtn) return;
+
+    logoutBtn.addEventListener("click", async () => {
+      await fazerLogout();
+    });
+  }, 300);
+}
